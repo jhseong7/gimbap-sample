c307369ea0ce778f8cc003b7f4c52739fbeb5e9b
jhseong7 <jhseong7@gmail.com>
2024-07-11 16:52:41 +0900

    Feat: update example of how to use db providers to the root GIMBAP module (#3)

--- db-connection/module.go
+++ db-connection/module.go
@@ -1,14 +1,13 @@
 package dbconnection
 
 import (
+	"github.com/jhseong7/gimbap"
 	"github.com/jhseong7/gimbap-sample/db-connection/db"
-	"github.com/jhseong7/gimbap/core"
-	"github.com/jhseong7/gimbap/provider"
 )
 
-var DbConnectionModule = core.DefineModule(core.ModuleOption{
+var DbConnectionModule = gimbap.DefineModule(gimbap.ModuleOption{
 	Name: "DbConnection",
-	Providers: []provider.ProviderDefinition{
+	Providers: []gimbap.ProviderDefinition{
 		*db.GormProvider,
 	},
 })
